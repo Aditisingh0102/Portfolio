b37fb48f0f15f550d2454ed1c1974f6e427feea3
Aditi Singh <your_email@example.com>
2025-09-25 02:08:55 +0530

    Updates

--- src/components/carouselStyles.js
+++
@@ -1,3 +0,0 @@
-// This file is required for react-slick carousel styles
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
--- src/pages/Projects.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ProjectsSection from "../components/ProjectsSection";
-
-function Projects() {
-  return <ProjectsSection />;
-}
-
-export default Projects;
